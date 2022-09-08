12f714b0e0cbfe2db1f50687fc7e0284d2a2002d
supachok jrirarojkul <supachok8728@gmail.com>
2022-09-08 20:06:47 +0700

    4.3

--- src/main/java/com/example/lab008/Lab008Application.java
+++ src/main/java/com/example/lab008/Lab008Application.java
@@ -2,12 +2,28 @@ package com.example.lab008;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Lab008Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Lab008Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/event")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 
 }
