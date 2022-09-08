40ccfd228fe759d4b0e503f4e4bf88b3a52be59a
supachok jrirarojkul <supachok8728@gmail.com>
2022-09-08 15:36:31 +0700

    3.2

--- src/main/java/com/example/lab008/contoller/EventController.java
+++ src/main/java/com/example/lab008/contoller/EventController.java
@@ -93,8 +93,12 @@ public class EventController {
         page = page == null?1:page;
         Integer firstIndex = (page - 1) * perPage;
         List<Event> output = new ArrayList<>();
-        for(int i = firstIndex; i < firstIndex + perPage ; i++) {
-            output.add(eventList.get(i));
+        try{
+            for(int i = firstIndex; i < firstIndex + perPage ; i++){
+                output.add(eventList.get(i));
+            }
+        }catch (IndexOutOfBoundsException ex){
+            return ResponseEntity.ok(output);
         }
         return ResponseEntity.ok(output);
     }
